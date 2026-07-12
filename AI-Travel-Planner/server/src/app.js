beaebfa6f61f7c06775e3ddbc30408b4ebfbb986
Gourab Polai <gourabpolai@gmail.com>
2026-07-12 22:42:28 +0530

    Day:6 Backend setup

---
+++ AI-Travel-Planner/server/src/app.js
@@ -0,0 +1,26 @@
+const healthRoutes = require("./routes/health.routes");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+/* -------------------- Middleware -------------------- */
+
+app.use(helmet());
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+/* -------------------- Routes -------------------- */
+
+app.use("/api/health", healthRoutes);
+
+module.exports = app;
